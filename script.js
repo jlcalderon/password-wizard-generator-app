41f7cbde1d76360038f894db744fe4f4da30129a
JLC <jlc@annonymous.com>
2020-07-21 23:52:32 -0500

    Clean up the code readability and logic with functions

--- script.js
+++ script.js
@@ -9,84 +9,95 @@ const passwordMaxChar = 128;
 //Fixing a constant value of th minimun characters allow in the generator app
 const passwordMinChar = 8;
 //Declare Capital Letters Array length of 25
-const capitalLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-console.log(capitalLetter);
+const capitalLetterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+console.log(capitalLetterArray);
 /*Declare Lower Case Letters Array taking the capitalLetter Array 
 and changing to lowecase, length remain the same 25 */
-let LowerCaseLetter = capitalLetter.toString().toLocaleLowerCase('en-US').split(",");
-console.log(LowerCaseLetter);
+const LowerCaseLetterArray = capitalLetterArray.toString().toLocaleLowerCase('en-US').split(",");
+console.log(LowerCaseLetterArray);
 //Declare Special Characters Array length is 19
-let specialChar = ["!", "#", "$", "%", "*", "[", "]", "/", ".", ",", ";", ":", "-", "+", "?", "¿", "&", "@", "€"];
-console.log(specialChar);
+const specialCharArray = ["!", "#", "$", "%", "*", "[", "]", "/", ".", ",", ";", ":", "-", "+", "?", "¿", "&", "@", "€"];
+console.log(specialCharArray);
 //Declare Numbers Array length is 10
-let numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(numbersArray);
+//Declaration of variables to store user settings from confirms and prompts
+// lenghtChar config the lenght in characters of the new password string this has a value of 0 by default
+let lenghtChar = 0;
+// specialChars config wether or not the user wants to add special characters to the password string this has a value of false by default
+let specialChars = false;
+// mayus config wether or not the user wants to add capital letters to the password string this has a value of false by default
+let mayus = false;
+// lowercase config wether or not the user wants to add lower case letters to the password string this has a value of false by default
+let lowercase = false;
+// numberChars config wether or not the user wants to add numbers to the password string this has a value of false by default
+let numberChars = false;
+/* countEvenCharacters config and even percentage of characters set up by the user to
+make the 100% of the password string populated with even percentages of:
+specialChars, mayus, lowercase, numbers
+this variable value is set to 0 by default
+*/
+let countEvenCharacters = 0;
 
 /********************Event Click of the button element on this app**************************/
 //Adding event listener to the button element to execute our code on user's click
 actionBtn.addEventListener("click", () => {
-    //First storing user input how many characters long they want their password with a prompt
-    let userInputChar = prompt("How long in characters would you like your new password?");
-    //Evaluating user's input narrowing to a minimun of 8 chars and max of 128 characters
-    if (userInputChar < passwordMinChar) {
-        //in case of user's input os lower than the app minimun display this alert
-        alert("Sorry your lenght in characters for the new password desired is not safe, to be secure your new password must be at least 8 characters long");
-    } else if (userInputChar > passwordMaxChar) {
-        //in case of user's input os greater than the app maximun display this alert
-        alert("Sorry your lenght in characters for the new password desired is out of bound, to be secure your new password must have a maximun length of 128 characters");
+    //First seting up how many characters long the password is going to be
+    setLenghtOfNewPassword();
+
+    //Ask the user to add special characters to the new password string
+    specialChars = confirm("Would you like to add special characters?");
+    //Evaluate users input of special characters
+    if (specialChars === true) {
+        //Add 1 to the counter of even percentages
+        countEvenCharacters++;
     }
-    //In the case of the user's input is correct acording to this app criteria, proceed here
-    else {
-        //Ask again for user's input let user decide wether or not they want to include special characters to the new password
-        let special = confirm("Would you like to add special characters");
-        //Evaluate users input of special characters
-        if (special === true) {
-            //In case user's response is positive do the following
 
-            /*/First grab the user desired password lenght and divided into 4,
-            Why 4? because we want equal parts of chracter 
-            from our predifined arrays to populate the new password*/
-            let specialCharCount = userInputChar / 4;
-            /*Making a loop of specialCharCount to
-            add a 1/4 fraction of special characters randomly taken from 
-            the array of special characters to the passwordString*/
-            for (let i = 0; i < specialCharCount; i++) {
-                //take random chars from the Array of special Chars wich length is 19
-                /*Note if -1 is not added to the max value in the getRandomIntInclusive
-                 function parameter you are going to get values undefined on your new password*/
-                let elementFromSpecialChars = getRandomIntInclusive(0, specialChar.length - 1);
-                passwordString = passwordString + specialChar[elementFromSpecialChars];
-            }
-            //And Continue with the next question to the user
-            let mayus = confirm("Would you like to add capital letters?");
-            //Evaluate users input of capital letters
-            if (mayus === true) {
-                //In case user's response is positive do the following
-
-                /*/First grab the user desired password lenght and divided into 4,
-            Why 4? because we want equal parts of chracters 
-            from our predifined arrays to populate the new password*/
-                let mayusCharCount = userInputChar / 4;
-                /*Making a loop of mayusCharCount to
-                add a 1/4 fraction of capital letters randomly taken from 
-                the array of capitalLetter to the passwordString*/
-                for (let m = 0; m < mayusCharCount; m++) {
-                    //take random chars from the Array of capital letters wich length is 25
-                    /*Note if -1 is not added to the max value in the getRandomIntInclusive
-                    function parameter you are going to get values undefined on your new password*/
-                    let elementFromCapitalLetter = getRandomIntInclusive(0, capitalLetter.length - 1);
-                    passwordString = passwordString + capitalLetter[elementFromCapitalLetter];
-                }
-
-            }
-
-        }
-        //Evaluate user's negative input of special characters 
-        else {
-            //Just Continue with the next question and don't add special characters to the new password
-
-        }
+    //Ask the user to add capital letters to the new password string
+    mayus = confirm("Would you like to add capital letters?");
+    //Evaluate users input of capital letters
+    if (mayus === true) {
+        //Add 1 to the counter of even percentages
+        countEvenCharacters++;
     }
+
+    //Ask the user to add lower case letters to the new password string
+    lowercase = confirm("Would you like to add lower case letters?");
+    //Evaluate users input of lower case letters
+    if (lowercase === true) {
+        //Add 1 to the counter of even percentages
+        countEvenCharacters++;
+    }
+
+    //Ask the user to add numbers to the new password string
+    numberChars = confirm("Would you like to add numbers to the new password?");
+    //Evaluate users input of numbers
+    if (numberChars === true) {
+        //Add 1 to the counter of even percentages
+        countEvenCharacters++;
+    }
+
+    // Call out the function to add special characters to new password string
+    if (specialChars === true) {
+        addSpecialCharacter();
+    }
+
+
+    //Call out the function to add Uppercase characters to the new password string
+    if (mayus === true) {
+        addCapitalLetters();
+    }
+
+    //Call out the function to add Lowercase characters to the new password string
+
+
+    //Call out the function to add Numbers to the new password string
+
+
+    //Output the new password string in the textarea element
+    document.getElementById("txtarea-password").textContent = passwordString;
+
+
 
 });
 
@@ -95,4 +106,54 @@ function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+/***************************** Functions and methods of this app ***************************/
+function setLenghtOfNewPassword() {
+    lenghtChar = prompt("How long in characters would you like your new password?");
+    //Evaluating user's input narrowing to a minimun of 8 chars and max of 128 characters
+    if (lenghtChar < passwordMinChar) {
+        //in case of user's input os lower than the app minimun display this alert
+        alert("Sorry your lenght in characters for the new password desired is not safe, to be secure your new password must be at least 8 characters long");
+    } else if (lenghtChar > passwordMaxChar) {
+        //in case of user's input os greater than the app maximun display this alert
+        alert("Sorry your lenght in characters for the new password desired is out of bound, to be secure your new password must have a maximun length of 128 characters");
+    } else {
+        alert("Great! Your desired character length meet the expectation of a secure password. Please proceed to select next option");
+    }
+    console.log(`Password length is going to be: ${lenghtChar}`);
+}
+
+function addSpecialCharacter() {
+    /*/First grab the user desired password lenght and divided into countEvenCharacters,
+    Why? because we want equal parts of chracter 
+    from our predifined arrays to populate the new password*/
+    let specialCharCount = lenghtChar / countEvenCharacters;
+    /*Making a loop of specialCharCount to
+    add a fraction of special characters randomly taken from 
+    the array of special characters to the passwordString*/
+    for (let i = 0; i < specialCharCount; i++) {
+        //takes random chars from the Array of special Chars wich length is 19
+        /*Note if -1 is not added to the max value in the getRandomIntInclusive
+         function parameter you are going to get values undefined on your new password*/
+        let elementFromSpecialChars = getRandomIntInclusive(0, specialCharArray.length - 1);
+        passwordString = passwordString + specialCharArray[elementFromSpecialChars];
+    }
+}
+
+function addCapitalLetters() {
+    /*/First grab the user desired password lenght and divided into countEvenCharacters,
+    Why? because we want equal parts of chracter 
+    from our predifined arrays to populate the new password*/
+    let capitalL = lenghtChar / countEvenCharacters;
+    /*Making a loop of capitalL to
+    add a fraction of capital letters randomly taken from 
+    the array of capital letters to the passwordString*/
+    for (let m = 0; m < capitalL; m++) {
+        //takes random chars from the Array of capital letters wich length is 25
+        /*Note if -1 is not added to the max value in the getRandomIntInclusive
+         function parameter you are going to get values undefined on your new password*/
+        let elementFromCapitalLetter = getRandomIntInclusive(0, capitalLetterArray.length - 1);
+        passwordString = passwordString + capitalLetterArray[elementFromCapitalLetter];
+    }
 }
